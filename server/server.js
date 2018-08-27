75273e907cdbbd341451b4946b2f84c98e0cbc69
Wastabir Rajvee Alam <wastabirrajveealamsa_cse10@its.edu.in>
2018-08-27 18:37:28 -0400

    Added POST /todo route and refactoring mongoose

--- server/server.js
+++ server/server.js
@@ -1,72 +1,28 @@
-const mongoose=require("mongoose");
-mongoose.Promise=global.Promise;
-mongoose.connect("mongodb://localhost:27017/TODOApp");
+var express = require('express');
+var bodyParser = require('body-parser');
 
-var TODO=mongoose.model("TODO",{
-    text:{
-     type: String,
-     required:true,
-     trim:true,
-     minlength:1
-    },
-    completed:{
-        type: Boolean,
-        default: false
-    },
-    completedAt:{
-        type: Number,
-        default: null
-    }
+var {mongoose} = require('./db/mongoose');
+var {Todo} = require('./models/todo');
+var {User} = require('./models/user');
+
+var app=express();
+app.use(bodyParser.json());
+
+app.post("/todos",(req,res)=>{
+   
+    var newTODO=new Todo({
+        text:req.body.text
+    });
+    newTODO.save().then((doc)=>{
+       res.send(doc);
+    }).catch((err)=>{
+        res.send(err);
+    });
+    console.log( req.body);
+    console.log(typeof req.body);
+    console.log("-------------------------------------------");
 });
-var User=mongoose.model("User",{
-    email:{
-     type: String,
-     required:true,
-     trim:true,
-     minlength:1
-    }
+var port= process.env.port || 3000;
+app.listen(port,()=>{
+console.log("Server started at port 3000");
 });
-
-var newUser=new User({
-    email:"wastabir.alam@gmail.com"
-});
-
-newUser.save().then((doc)=>{
-    console.log("Saved the doc",doc);
-}).catch((err)=>{
-    console.log("Unable to save the doc");
-});
-
-
-// var newTODO=new TODO({
-//     text:"Cook dinner"
-// });
-// newTODO.save().then((doc)=>{
-//   console.log("Saved the doc",doc)
-// }).catch((err)=>{
-//     console.log("Unable to save the doc");
-// });
-
-// var newTODO1=new TODO({
-//     text:"Cook dinner",
-//     completed:false,
-//     completedAt:12344
-// });
-// newTODO1.save().then((doc)=>{
-//     console.log("Saved the doc",doc)
-// }).catch((err)=>{
-//     console.log("Unable to save the doc",err);
-// });
-
-// var newTODO1=new TODO({
-    
-//             text:"Cook dinner",
-//             completed:false,
-//             completedAt:12344
-     
-// });
-// newTODO1.save().then((doc)=>{
-//     console.log("Saved the doc",doc)
-// }).catch((err)=>{
-//     console.log("Unable to save the doc",err);
-// });
